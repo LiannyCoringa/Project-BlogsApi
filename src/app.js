41aaf538dd77a77ba9143e7d1ef47ecd7edcf528
Lianny Coringa <lianny_costa@hotmail.com>
2023-11-02 09:39:35 -0300

    Requisito 8 - criando a rota post /categories

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const loginController = require('./controllers/login');
 const userController = require('./controllers/userController');
+const categoryController = require('./controllers/categoryController');
 const loginMiddleware = require('./middlewares/login');
 const userMiddleware = require('./middlewares/user');
 const auth = require('./middlewares/auth');
+const category = require('./middlewares/category');
 
 // ...
 
@@ -22,6 +24,8 @@ app.get('/user', auth, userController.findAll);
 app.post('/user', userMiddleware, userController.create);
 
 app.get('/user/:id', auth, userController.findById);
+
+app.post('/categories', auth, category, categoryController.create);
 
 // ...
 
---
+++ src/controllers/categoryController.js
@@ -0,0 +1,12 @@
+const categoryService = require('../services/category.service');
+
+const create = async (req, res) => {
+  const { name } = req.body;
+  const { status, data } = await categoryService.create({ name });
+
+  return res.status(status).json(data);
+};
+
+module.exports = {
+  create,
+};
